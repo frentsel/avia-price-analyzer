a5babefd50fb7688cf03015265a77b53fe4e1f3d
afrentsel <afrentsel@bitbucket.org>
2019-06-14 16:34:09 +0300

    added jsGrid

--- js/main.js
+++ js/main.js
@@ -7,37 +7,60 @@ const foo = (f, t, res = [], limit = 3) => {
 
 	if (!f.length) return res;
 
-	const _from = f.shift();
-	const _to = t
-		.filter(el => getDate(el) > getDate(_from))
+	const from = f.shift();
+	const to = t
+		.filter(el => getDate(el) > getDate(from))
 		.slice(0, limit)
 		.sort((a, b) => a.price.amount - b.price.amount)
 		.shift();
 
-	if (!_to) return res;
+	if (!to) return res;
 
-	const { currencyCode: cur } = _from.price;
-	const { amount: fPrice } = _from.price;
-	const { amount: tPrice } = _to.price;
-	let { departureDate: fDate } = _from;
-	let { departureDate: tDate } = _to;
-	fDate = new Date(fDate).toString().split(' ').slice(0, 4).reverse().join('/');
-	tDate = new Date(tDate).toString().split(' ').slice(0, 4).reverse().join('/');
 	res.push({
-		sum: fPrice + tPrice,
-		price: `${fPrice} + ${tPrice}`,
-		form: fDate,
-		to: tDate,
-		days: dateDiff(_from, _to)
+		from,
+		to,
+		sum: from.price.amount + to.price.amount,
+		days: dateDiff(from, to)
 	});
 	return foo(f, t, res);
 };
+
+const tpl = {
+	date: (date) => {
+		const [wDay, month, day, year] = new Date(date).toString().split(' ');
+		return `${day} ${month} ${year}, ${wDay}`;
+	}
+};
+
+const renderDataGrid = (data) => {
+	const fDirection = `${data[0].from.departureStation} ✈ ${data[0].from.arrivalStation}`;
+	const tDirection = `${data[0].to.departureStation} ✈ ${data[0].to.arrivalStation}`;
+	const currency = data[0].from.price.currencyCode;
+	$('#jsGrid').jsGrid({
+		width: '100%',
+		height: '400px',
+		sorting: true,
+		paging: true,
+		data,
+		fields: [
+			{ name: 'sum', title: `Total sum (${currency})`, type: 'number', width: 50, sorting: true },
+			{ name: 'from.price.amount', title: `${fDirection}, Price (${currency})`, type: 'text', width: 50, sorting: true },
+			{ name: 'to.price.amount', title: `${tDirection}, Price (${currency})`, type: 'text', width: 50, sorting: true },
+			{ name: 'from.departureDate', title: 'Departure time', type: 'text', width: 100, sorting: true, sorter: 'date', itemTemplate: tpl.date },
+			{ name: 'to.departureDate', title: 'Arrival time', type: 'text', width: 100, sorting: true, sorter: 'date', itemTemplate: tpl.date },
+			{ name: 'days', title: 'Total days', type: 'number', width: 50, sorting: true }
+		]
+	});
+}
 
 const analize = (city = 'gdansk') => {
 	fetch(`data/${city}.json`)
 		.then((res) => res.json())
 		.then(({ outboundFlights: from, returnFlights: to }) => {
 			const result = foo(from, to);
-			console.log(result.sort((a, b) => a.sum - b.sum));
+			const data = result.sort((a, b) => a.sum - b.sum);
+			renderDataGrid(data);
 		});
 }
+
+analize('gdansk');
